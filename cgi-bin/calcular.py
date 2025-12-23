b005c6ad02d24afdc81023b4ba2ece8986ec522b
Marcelo-Flores-Solis <mfloresso@unsa.edu.pe>
2025-12-22 22:05:26 -0500

    Logica del regex

--- cgi-bin/calcular.py
+++ cgi-bin/calcular.py
@@ -6,3 +6,9 @@ import cgitb
 # habilitar el modo de depuración
 cgitb.enable()
 print("Content-Type: text/html\n") 
+
+# expresiones regulares
+# parentesis opcionales: \(? ... \)?
+# operadores permitidos: [\+\-\*\/]
+# operandos (\d+\.?\d*)
+
