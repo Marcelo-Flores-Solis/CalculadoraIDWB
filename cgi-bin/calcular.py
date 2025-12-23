23a3b8258546e7a9d0845059a0ef0f05690f8b2d
Marcelo-Flores-Solis <mfloresso@unsa.edu.pe>
2025-12-22 22:09:46 -0500

    Logica terminada del regex

--- cgi-bin/calcular.py
+++ cgi-bin/calcular.py
@@ -9,6 +9,17 @@ print("Content-Type: text/html\n")
 
 # expresiones regulares
 # parentesis opcionales: \(? ... \)?
-# operadores permitidos: [\+\-\*\/]
+# operadores permitidos: ([\+\-\*/])
 # operandos (\d+\.?\d*)
+# espacios opcionales: \s*
+# primer y tercer bloque : \s* \(? \s* (\d+\.?\d*) \s* \)? \s*
+# segundo bloque: ([\+\-\*/])
+
+pattern = r"^\s*\(?\s*(\d+\.?\d*)\s*\)?\s*([\+\-\*/])\s*\(?\s*(\d+\.?\d*)\s*\)?\s*$"
+
+match = re.match(pattern, operacion_raw)
+
+resultado_html = ""
+
+
 
